4981d03f657e6f64d40b32e9d785ad1a6c09709f
Carloslt <carloslt.proyectos@gmail.com>
2023-07-31 17:40:58 +0200

    formatted

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,14 +1,14 @@
 
- const button = document.querySelector('.header__button')
- const nav    = document.querySelector('.nav')
- button.addEventListener('click',()=>{
-     nav.classList.toggle('active')
- })
+const button = document.querySelector('.header__button')
+const nav = document.querySelector('.nav')
+button.addEventListener('click', () => {
+  nav.classList.toggle('active')
+})
 
- const li     = document.querySelectorAll('.nav__li')
+const li = document.querySelectorAll('.nav__li')
 li.forEach(el => {
-    el.addEventListener('click',()=>{
-        nav.classList.remove('active')
-    })
+  el.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
 
 })
